b62e43a2c1f6881223c4f61dcee67c047234db67
prasad <prasadbarsinge09@gmail.com>
2022-12-16 19:59:18 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,11 @@
+# database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_URL = 'sqlite:///todo.sqlite3'
+
+engine = create_engine(DB_URL, connect_args={'check_same_thread': False})
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+
+from sqlalchemy import Column, Integer, Boolean, Text
+from database import Base
+
+class Todo(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True)
+    task = Column(Text)
+    completed = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return '<Todo %r>' % (self.id)
